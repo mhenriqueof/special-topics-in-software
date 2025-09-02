04f0972072469cd90baf8d2fbcaeec2c58325316
Henrique Oliveira <mhenriqueof@outlook.com>
2025-09-02 11:59:34 -0300

    Update Race: refactor the class
    - add Enum
    - change constructor and methods

--- races/race.py
+++ races/race.py
@@ -1,20 +1,46 @@
 from abc import ABC, abstractmethod
+from enum import Enum
+
+class Alignment(Enum):
+    ANY = 'Any'
+    NEUTRAL = 'Neutral'
+    ORDER = 'Order'
 
 class Race(ABC):
-    def __init__(self, movement: int, infravision: int, alignment: str, skills: dict[str, str]):
-        self.movement = movement
-        self.infravision = infravision
-        self.alignment = alignment
-        self.skills = skills
+    """Abstract class for all races."""
+    def __init__(self, movement: int, infravision: int, alignment: Alignment):
+        self.__movement = movement
+        self.__infravision = infravision
+        self.__alignment = alignment
+   
         
+    @property
+    def movement(self) -> int:
+        return self.__movement
+        
+    @property
+    def infravision(self) -> int:
+        return self.__infravision
+
+    @property
+    def alignment(self) -> Alignment:
+        return self.__alignment
+    
+
     @abstractmethod
-    def story(self) -> str:
+    def overview(self) -> str:
+        """Returns a brief description of the race (origins, personality and adventures)."""
         pass
     
     @abstractmethod
-    def personality(self) -> str:
+    def talents(self) -> list[str]:
+        """Returns a list with all talents of the race (passive skills)."""
         pass
-    
-    @abstractmethod
-    def adventures(self) -> str:
-        pass
+
+
+    def __repr__(self) -> str:
+        return (f"{self.__class__.__name__}: "
+                f"movement={self.movement}, "
+                f"infravision={self.infravision}, "
+                f"alignment={self.alignment.value}")
+        
