70f6dffa8405d2fe31a5aed847c1789a617f7c26
Henrique Oliveira <mhenriqueof@outlook.com>
2025-09-09 10:37:43 -0300

    Update handy_functions.py
    Update HandyFunctions: get show_attributes() and reset_attributes() from character.py

--- handy_functions.py
+++ handy_functions.py
@@ -1,5 +1,7 @@
 class HandyFunctions:
-    """Class with methods for repetitive actions."""
+    """Class with methods that are used in many files."""
+    
+    # General methods
     @staticmethod
     def input_int(string: str = '') -> int:
         while True:
@@ -8,3 +10,32 @@ class HandyFunctions:
                 return int(value)
             else:
                 print("Type a number.")
+
+    # Character methods
+    @staticmethod
+    def show_attributes(attributes: dict) -> None:
+        """Shows the character's attributes, their values and the modifier."""
+        def attribute_modifier(value: int) -> str:
+            """Returns a modifier value for the attributes."""
+            if value >= 19: return '+4'
+            elif value >= 17: return '+3'
+            elif value >= 15: return '+2'
+            elif value >= 13: return '+1'
+            elif value >= 9: return ' 0'
+            elif value >= 6: return '-1'
+            elif value >= 4: return '-2'
+            elif value >= 2: return '-3'
+            return ' 0'
+        print()
+        print("-" * 25)
+        for attribute, value in attributes.items():
+            print(f"{value:>2} <> {attribute_modifier(value)} <> {attribute}")
+        print("-" * 22)
+        print()
+    
+    @staticmethod
+    def reset_attributes(attributes: dict) -> None:
+        """Changes all attributes values to 0."""
+        for attribute in attributes:
+            attributes[attribute] = 0
+            
