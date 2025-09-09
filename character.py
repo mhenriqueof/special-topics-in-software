62a0955e5be20283b061a23b6929316257f3c8d9
Henrique Oliveira <mhenriqueof@outlook.com>
2025-09-09 10:25:07 -0300

    Update Character: refactor class
    - moved show_attributes() and reset_attributes() to handy_functions.py.
    - now the class will be instanced with all constructors parameters filled.
    - no more subclasses.
    - now only __repr__.

--- character.py
+++ character.py
@@ -1,78 +1,43 @@
+from races.race import Race
+from classes.klass import Klass
+
 class Character:
-    """Represents a character with their name and attributes."""
-    def __init__(self, name: str):
-        self.__name: str = name
-        self.__attributes = {
-            "STR (Strength)": 0,
-            "DEX (Dexterity)": 0,
-            "CON (Constitution)": 0,
-            "INT (Intelligence)": 0,
-            "WIS (Wisdom)": 0,
-            "CHA (Charisma)": 0
-            }
-        self.__race = None
-        self.__classs = None # "classs" to avoid class of Python
-        self.__subclasss = None
+    """Represents a character with their name, attributes, race and class."""
+    def __init__(self, name: str, attributes: dict, race: Race, klass: Klass):
+        self.__name = name
+        self.__attributes = attributes
+        self.__race = race
+        self.__klass = klass # "K" to avoid 'class' of Python
         
-    # __name
     @property
     def name(self) -> str:
         return self.__name
     @name.setter
     def name(self, name: str) -> None:
         self.__name = name
-        
-    # __attributes
+
     @property
     def attributes(self) -> dict[str, int]:
         return self.__attributes
     @attributes.setter
     def attributes(self, attributes: dict[str, int]) -> None:
         self.__attributes = attributes
-        
-    # __race
+
     @property
     def race(self):
         return self.__race
     @race.setter
     def race(self, race) -> None:
         self.__race = race
-        
-    # __classs
-    @property
-    def classs(self):
-        return self.__classs
-    @classs.setter
-    def classs(self, classs) -> None:
-        self.__classs = classs
 
-    # __subclasss
     @property
-    def subclass(self):
-        return self.__subclass
-    @subclass.setter
-    def subclass(self, classs) -> None:
-        self.__subclass = classs
-    
-    # General methods
-    def show_attributes(self) -> None:
-        """Shows the character's attributes and their values."""
-        print()
-        print("-" * 25)
-        for attribute, value in self.attributes.items():
-            print(f"{value:>2} <> {attribute}")
-        print("-" * 22)
-        print()
-    
-    def reset_attributes(self) -> None:
-        """Changes all attributes values to 0"""
-        for attribute in self.attributes:
-            self.attributes[attribute] = 0
+    def klass(self):
+        return self.__klass
+    @klass.setter
+    def klass(self, klass) -> None:
+        self.__klass = klass
 
-    # Magic methods
-    def __str__(self) -> str: # "Readable" representation
-        return f"- {self.name} -\nStats: {self.attributes}"
-    
-    def __repr__(self) -> str: # "Debug" representation
+    # Magic method 
+    def __repr__(self) -> str:
         return f"Character(name='{self.name}', stats={self.attributes})"
     
